daa77997b18de42f967909f6e4138d43f9e7b425
tomerschwartz2411 <tomerschwartz2411@gmail.com>
2023-04-28 20:01:43 +0300

    Add basic service example and application files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module httpg.go
+
+go 1.20
---
+++ httpg.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	// Handle HTTP requests to "/" and serve the web resources inside the application folder ( index.html / images and so on)
+	http.Handle("/", http.FileServer(http.Dir("application")))
+	//Error Handling
+	webappListener := http.ListenAndServe(":80", nil)
+	if webappListener != nil {
+		fmt.Printf("Unable to bind port  %v\n", webappListener)
+	} else {
+		fmt.Println("Application is listening on port 8080")
+	}
+}
