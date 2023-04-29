42bed5e995768d14acc0e05112eb07c42c7b5b17
tomerschwartz2411 <tomerschwartz2411@gmail.com>
2023-04-29 10:48:32 +0300

    Change ec2 creation count to 2

--- ec2-creation.tf
+++ ec2-creation.tf
@@ -1,6 +1,6 @@
 //ec2 instance resource creation, all values are refered inside variables.tf 
 resource "aws_instance" "ec2_instance" {
-  count = 1
+  count = 2
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
